627f4cd4c9ddabc8ef920959d642c7a1978b495a
itohan <itohanidubor@gmail.com>
2025-12-10 23:17:49 +0100

    Update Apollo client to use environment variables

--- client/app/graphql/client.js
+++ client/app/graphql/client.js
@@ -2,6 +2,6 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  uri: "http://localhost:4000/",
   cache: new InMemoryCache(),
 });
