9ef0f05391d10ff14083b930e50cbf76eeacc451
itohan <itohanidubor@gmail.com>
2025-12-11 07:41:17 +0100

    Add GraphQL URL logging

--- client/app/graphql/client.js
+++ client/app/graphql/client.js
@@ -1,7 +1,12 @@
 "use client";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+const graphqlUrl =
+  process.env.NEXT_PUBLIC_GRAPHQL_URL || "http://localhost:4000/";
+
+console.log("🔗 GraphQL URL being used:", graphqlUrl);
+
 export const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_GRAPHQL_URL || "http://localhost:4000/",
+  uri: graphqlUrl,
   cache: new InMemoryCache(),
 });
