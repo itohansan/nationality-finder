c5595d9c8642c6d9da9135753a95365a0e4f044d
itohan <itohanidubor@gmail.com>
2025-12-11 07:59:42 +0100

    Remove console.log that breaks build

--- client/app/page.js
+++ client/app/page.js
@@ -2,9 +2,7 @@
 
 import SearchForm from "./components/SearchForm";
 import { ApolloProvider } from "@apollo/client/react";
-import { client } from "./graphql/client"; // Import from your client.js file
-
-console.log("🚀 Page loaded, Apollo Client URI:", client.link.options.uri);
+import { client } from "./graphql/client";
 
 export default function Page() {
   return (
