48233e3f09c0c710652384d72dbd27327310c531
itohan <itohanidubor@gmail.com>
2025-12-11 08:50:23 +0100

    Fix Apollo Client link configuration

--- client/app/graphql/client.js
+++ client/app/graphql/client.js
@@ -1,12 +1,16 @@
 "use client";
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
-const graphqlUrl =
-  process.env.NEXT_PUBLIC_GRAPHQL_URL || "http://localhost:4000/";
+const httpLink = new HttpLink({
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_URL || "http://localhost:4000/",
+});
 
-console.log("🔗 GraphQL URL being used:", graphqlUrl);
+console.log(
+  "🔗 GraphQL URL being used:",
+  process.env.NEXT_PUBLIC_GRAPHQL_URL || "http://localhost:4000/"
+);
 
 export const client = new ApolloClient({
-  uri: graphqlUrl,
+  link: httpLink,
   cache: new InMemoryCache(),
 });
