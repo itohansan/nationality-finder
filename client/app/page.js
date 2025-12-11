f5842db47251e899640c854ff1cafa3e96353928
itohan <itohanidubor@gmail.com>
2025-12-11 07:54:36 +0100

    Fix: Use client from graphql/client.js with environment variable

--- client/app/page.js
+++ client/app/page.js
@@ -1,28 +1,17 @@
 "use client";
 
 import SearchForm from "./components/SearchForm";
-import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { ApolloProvider } from "@apollo/client/react";
-import { ApolloLink } from "@apollo/client/core";
-import { HttpLink } from "@apollo/client/link/http";
+import { client } from "./graphql/client"; // Import from your client.js file
 
-const client = new ApolloClient({
-  link: new HttpLink({
-    uri: "http://localhost:4000",
-  }),
-  cache: new InMemoryCache(),
-});
+console.log("🚀 Page loaded, Apollo Client URI:", client.link.options.uri);
 
 export default function Page() {
   return (
     <ApolloProvider client={client}>
       <main className="p-10">
-        {/* <h1 className="text-2xl font-bold mb-4">
-          Nationality Finder (GraphQL)
-        </h1> */}
         <SearchForm />
       </main>
     </ApolloProvider>
   );
 }
-// Updated Thu Dec 11 07:30:39 WAT 2025
